2568f370cedf601ced4bb2d4c4776e706f01193f
Dudous <sampaioeeduardo36@gmail.com>
2024-09-02 13:28:18 -0300

    end :(

--- Game.cs
+++ Game.cs
@@ -51,8 +51,9 @@ public class Game
 
     public void Atack(){
         while(!CheckNull(yourRoundTeam) && !CheckNull(enemyRoundTeam)){
-            -
+            
         }
+        throw new NotImplementedException();
     }
 
     public void GenerateEnemy()
